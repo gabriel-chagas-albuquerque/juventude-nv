4f201f7b8b54a5ef14070f543e7cd3777ffa31e5
gabriel-chagas-albuquerque <gabrieldafortaleza0100@gmail.com>
2026-04-29 20:33:09 -0300

    Remove hardcoded secrets and fix environment variable usage

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,8 +2,8 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.VITE_SANITY_PROJECT_ID || '1mpxey5n',
-    dataset: process.env.VITE_SANITY_DATASET || 'production'
+    projectId: process.env.VITE_SANITY_PROJECT_ID as string,
+    dataset: process.env.VITE_SANITY_DATASET as string
   },
   deployment: {
     appId: 'e38w0fe4g353w85sl6jevmo8',
--- sanity.config.ts
+++ sanity.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   name: 'default',
   title: 'Juventude NV Admin',
 
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || '1mpxey5n',
-  dataset: import.meta.env.VITE_SANITY_DATASET || 'production',
+  projectId: (import.meta.env.VITE_SANITY_PROJECT_ID as string),
+  dataset: (import.meta.env.VITE_SANITY_DATASET as string),
 
   plugins: [deskTool()],
 
